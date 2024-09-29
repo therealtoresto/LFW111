2c14a83122c145130fdd0aacb4c5206a1ec55298
therealtoresto <therealtoresto@gmail.com>
2024-09-29 22:55:42 +0300

    feat: improved ui

--- static/app.js
+++ static/app.js
@@ -1,16 +1,12 @@
-const API = "http://localhost:3000/"
+const API = "http://localhost:3000"
 
-const mockData = [
-  {id: 'A1', name: 'Vacuum Cleaner', rrp: '99.99', info: 'The most powerful vacuum in the world.'},
-  {id: 'A2', name: 'Leaf Blower', rrp: '303.33', info: 'This product will blow your socks off.'},
-  {id: 'B1', name: 'Chocolate Bar', rrp: '22.40', info: 'Deliciously overpriced chocolate.'}
-]
-const populateProducts = async () => {
+const populateProducts = async (category) => {
   const products = document.querySelector('#products')
   products.innerHTML = ''
-  const res = await fetch(API)
+  const res = await fetch(`${API}/${category}`)
   const data = await res.json()
-  for (const product of mockData) {
+
+  for (const product of data) {
     const item = document.createElement('product-item')
     for (const key of ['name', 'rrp', 'info']) {
       const span = document.createElement('span')
@@ -22,14 +18,16 @@ const populateProducts = async () => {
   }
 }
 
-document.querySelector('#fetch').addEventListener('click', async () => {
-  await populateProducts()
+const category = document.querySelector('#category')
+
+category.addEventListener('input', async ({ target }) => {
+  await populateProducts(target.value)
 })
 
 customElements.define('product-item', class Item extends HTMLElement {
   constructor() {
     super()
-    const itemTmpl = document.querySelector('#item').content.cloneNode(true)
-    this.attachShadow({mode: 'open'}).appendChild(itemTmpl)
+    const itemTmpl = document.querySelector('#item').content
+    this.attachShadow({mode: 'open'}).appendChild(itemTmpl.cloneNode(true))
   }
 })
