5d5cf64fe13a11a7eeb0f9f78aaad320eba9ae7b
therealtoresto <therealtoresto@gmail.com>
2024-10-05 10:36:42 +0300

    feat: added mocking data

--- 01-mock-srv/routes/confectionery/index.mjs
+++ 01-mock-srv/routes/confectionery/index.mjs
@@ -7,7 +7,13 @@ const data = [
     info: "Delicious overpriced chocolate.",
   },
 ];
-export default async function (fastify) {
+export default async function (fastify, opts) {
   fastify.get("/", async function (request, reply) {
-    return data
-})};
+    return data;
+  });
+
+  fastify.post("/", async function (request, reply) {
+    fastify.mockDataInsert(request, opts.prefix.slice(1), data);
+    return data;
+  });
+};
--- 01-mock-srv/routes/electronics/index.mjs
+++ 01-mock-srv/routes/electronics/index.mjs
@@ -4,7 +4,7 @@ const data = [
     id: "A1",
     name: "Vacuum Cleaner",
     rrp: "99.99",
-    info: "The suckiest vacuum in the world.",
+    info: "The most powerful vacuum in the world.",
   },
   {
     id: "A2",
@@ -14,7 +14,13 @@ const data = [
   },
 ];
 
-export default async function (fastify) {
+export default async function (fastify,opts) {
   fastify.get("/", async function (request, reply) {
     return data;
-  })}
+  });
+  fastify.post("/", async function (request, reply) {
+    fastify.mockDataInsert(request, opts.prefix.slice(1), data);
+    return data
+  });
+}
+
