5e28a7a236d86b2a2819f46e0f6d913d16852cc3
therealtoresto <therealtoresto@gmail.com>
2024-10-13 22:50:58 +0300

    feat: improved cli tool

--- 02-cli-tool/bin/cmd.js
+++ 02-cli-tool/bin/cmd.js
@@ -3,30 +3,25 @@ import got from 'got';
 import { Command } from "commander";
 // Create a new Command Program
 const program = new Command();
-
-// Create a new Program
-program
-  .name("my-cli") // Set the name of the program
-  .description("Back office for My App") // Set the description
-  .version("1.0.0"); // Set the version
-
-// Parse the arguments from process.argv
-program.parse();
-
 const API = "http://localhost:3000";
 
 const usage = (msg = 'Back office for My App') => {
   console.log(`\n${msg}\n`);
-  console.log("Usage: cmd <ID> <AMOUNT>");
 };
 
-// Get the arguments from the command line
-const argv = process.argv.slice(2);
-// If there are no arguments, show the usage and exit
-if (argv.length < 2) {
-  usage();
-  process.exit(1);
-}
+// Create a command for adding a new order
+program
+  // Set the command name
+  .command("update")
+  // Set the argument ID to be required
+  .argument("<ID>", "Order ID")
+  // Set the argument AMOUNT to be required
+  .argument("<AMOUNT>", "Order Amount")
+  // Set the action to be executed when the command is run
+  .action(async (id, amount) => await updateItem(id, amount));
+
+// Parse the arguments from process.argv
+program.parse();
 
 // Update the order with the given ID
 async function updateItem(id,amount) {
