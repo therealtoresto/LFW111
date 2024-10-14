9af428cd38249e91cbf46ade8f7cdb2d08eb849b
therealtoresto <therealtoresto@gmail.com>
2024-10-14 07:48:38 +0300

    feat: add method for cli tool

--- 02-cli-tool/bin/cmd.js
+++ 02-cli-tool/bin/cmd.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
-import got from 'got';
+import { update } from "../src/utils.js";
 import { Command } from "commander";
 // Create a new Command Program
 const program = new Command();
-const API = "http://localhost:3000";
 
 const usage = (msg = 'Back office for My App') => {
   console.log(`\n${msg}\n`);
@@ -22,24 +21,3 @@ program
 
 // Parse the arguments from process.argv
 program.parse();
-
-// Update the order with the given ID
-async function updateItem(id,amount) {
-  usage(`Updating order ${id} with amount ${amount}`)
-  try {
-    if (isNaN(+amount)) {
-      usage("Error: <AMOUNT> must be a number");
-      process.exit(1);
-    }
-    // Use GOT to make a POST request to the API
-    await got.post(`${API}/orders/${id}`, {
-      json: { amount: +amount },
-    });
-    // Log the result to the console
-    usage(`Order ${id} updated with amount ${amount}`);
-  } catch (err) {
-    // If there is an error, log it to the console and exit
-    console.error(err.message);
-    process.exit(1);
-  }
-}
---
+++ 02-cli-tool/src/utils.js
@@ -0,0 +1,64 @@
+// Import GOT to make HTTP requests
+import { got } from "got";
+// Set the API URL
+const API = "http://localhost:3000";
+// Set the categories
+const categories = ["confectionery", "electronics"];
+
+// Log the usage of the command to the console
+export const log = (msg) => {
+  console.log(`\n${msg}\n`);
+};
+// Log the error to the console
+export const error = (msg) => {
+  console.error(`\n${msg}\n`);
+};
+
+// Update the order with the given ID
+export async function update(id, amount) {
+  log(`Updating order ${id} with amount ${amount}`);
+  try {
+    if (isNaN(+amount)) {
+      log("Error: <AMOUNT> must be a number");
+      process.exit(1);
+    }
+    // Use GOT to make a POST request to the API
+    await got.post(`${API}/orders/${id}`, {
+      json: { amount: +amount },
+    });
+    // Log the result to the console
+    log(`Order ${id} updated with amount ${amount}`);
+  } catch (err) {
+    // If there is an error, log it to the console and exit
+    error(err.message);
+    process.exit(1);
+  }
+}
+
+// Add a new order
+export async function add(...args) {
+  // Destructure the arguments
+  let [category, id, name, amount, info] = args;
+  log(`Adding item ${id} with amount ${amount}`);
+  try {
+    if (isNaN(+amount)) {
+      error(`Error: <AMOUNT> must be a number`);
+      process.exit(1);
+    }
+    // Use GOT to make a POST request to the API
+    await got.post(`${API}/${category}`, {
+      json: {
+        id,
+        name,
+        rrp: +amount,
+        info: info.join(" "),
+      },
+    });
+    // Log the result to the console
+    log(`Item "${id}:${name}" has been added to the ${category} category`);
+  } catch (err) {
+    // If there is an error, log it to the console and exit
+    error(err.message);
+    process.exit(1);
+  }
+}
