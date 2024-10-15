43836d017a9a01d58cde45479c135a92112c8d7b
Taras Bondaruk <therealtoresto@gmail.com>
2024-10-15 21:13:10 +0300

    feat: updated commands

--- 02-cli-tool/bin/cmd.js
+++ 02-cli-tool/bin/cmd.js
@@ -1,12 +1,36 @@
 #!/usr/bin/env node
-import { add, listCategories, listCategoryItems } from "../src/utils.js";
+import {
+  add,
+  listCategories,
+  listCategoryItems,
+  update,
+} from "../src/utils.js";
 import { Command } from "commander";
+
 // Create a new Command Program
 const program = new Command();
 
-const usage = (msg = 'Back office for My App') => {
-  console.log(`\n${msg}\n`);
-};
+// Create a new Program
+program
+  // Set the name of the program
+  .name("my-cli")
+  // Set the description
+  .description("Back office for My App")
+  // Set the version
+  .version("1.0.0")
+
+// Create a command for adding a updating order
+program
+  // Set the command name
+  .command("update")
+  // Set the command description
+  .description("Update an order")
+  // Set the argument ID to be required
+  .argument("<ID>", "Order ID")
+  // Set the argument AMOUNT to be required
+  .argument("<AMOUNT>", "Order Amount")
+  // Set the action to be executed when the command is run
+  .action(async (id, amount) => await update(id, amount));
 
 // Create a command for listing categories by IDs
 program
@@ -28,30 +52,30 @@ program
   .action(
     async (category, id, name, amount, info) =>
       await add(category, id, name, amount, info)
-  );
+);
 
-  // Create a command for listing categories
+// Create a command for listing categories
 program
-// Set the command name
-.command("list")
-// Set the command description
-.description("List categories")
-// Set the category to be optional
-.argument("[CATEGORY]", "Category to list IDs for")
-// Set the option to list all categories
-.option("-a, --all", "List all categories")
-// Set the action to be executed when the command is run
-.action(async (args, opts) => {
-  if (args && opts.all)
-    throw new Error("Cannot specify both category and 'all'");
-  if (opts.all || args === "all") {
-    listCategories();
-  } else if (args === "confectionery" || args === "electronics") {
-    await listCategoryItems(args);
-  } else {
-    throw new Error("Invalid category specified");
-  }
-});
+  // Set the command name
+  .command("list")
+  // Set the command description
+  .description("List categories")
+  // Set the category to be optional
+  .argument("[CATEGORY]", "Category to list IDs for")
+  // Set the option to list all categories
+  .option("-a, --all", "List all categories")
+  // Set the action to be executed when the command is run
+  .action(async (args, opts) => {
+    if (args && opts.all)
+      throw new Error("Cannot specify both category and 'all'");
+    if (opts.all || args === "all") {
+      listCategories();
+    } else if (args === "confectionery" || args === "electronics") {
+      await listCategoryItems(args);
+    } else {
+      throw new Error("Invalid category specified");
+    }
+  });
 
 // Parse the arguments from process.argv
 program.parse();
