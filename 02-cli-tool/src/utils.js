9ba13d7d138dcb9314903c4aa6981b5d78e4aca2
Taras Bondaruk <therealtoresto@gmail.com>
2024-10-15 21:16:44 +0300

    feat: updated methods

--- 02-cli-tool/src/utils.js
+++ 02-cli-tool/src/utils.js
@@ -1,38 +1,33 @@
 // Import GOT to make HTTP requests
 import { got } from "got";
 import {
-  displayTimestamp,
-  displayInfo,
+  log,
+  error,
   displayAmount,
+  displayCategory,
+  displayID,
+  displayInfo,
+  displayKey,
+  displayName,
+  displayRRP,
   displaySuccess,
   displayText,
-  displayID,
-} from './displays.js'
+  displayTimestamp
+} from "./displays.js";
+
 // Set the API URL
 const API = "http://localhost:8080";
+
 // Set the categories
 const categories = ["confectionery", "electronics"];
-
-// Log the usage of the command to the console
-export const log = (msg) => {
-  console.log(`\n${msg}\n`);
-};
-// Log the error to the console
-export const error = (msg) => {
-  console.error(`\n${msg}\n`);
-};
 
 // Update the order with the given ID
 export async function update(id, amount) {
   log(`${displayTimestamp()}`);
-  log(
-    `${displayInfo(`Updating Order`)} ${displayID(id)} ${displayText(
-      "with amount"
-    )} ${displayAmount(amount)}`
-  );
+  log(`${displayInfo(`Updating Order`)} ${displayID(id)} ${displayText("with amount")} ${displayAmount(amount)}`);
   try {
     if (isNaN(+amount)) {
-      error("<AMOUNT> must be a number");
+      error(" must be a number");
       process.exit(1);
     }
     // Use GOT to make a POST request to the API
@@ -41,10 +36,8 @@ export async function update(id, amount) {
     });
     // Log the result to the console
     log(
-      `${displaySuccess()} ${displayText("Order")} ${displayID(
-       id
-      )} ${displayText("updated with amount")} ${displayAmount(amount)}`
-    );
+      `${displaySuccess()} ${displayText("Order")} `
+      + `${displayID(id )} ${displayText("updated with amount")} ${displayAmount(amount)}`);
   } catch (err) {
     // If there is an error, log it to the console and exit
     console.log(err.message);
@@ -56,10 +49,12 @@ export async function update(id, amount) {
 export async function add(...args) {
   // Destructure the arguments
   let [category, id, name, amount, info] = args;
-  log(`Adding item ${id} with amount ${amount}`);
+  log(`${displayTimestamp()}`);
+  log(`${displayInfo(`Request to add item to category`)} ${displayCategory(category)}`);
+  log(`${displayText("Adding item")} ${displayID(id)} ${displayText("with amount")} ${displayAmount(`$${amount}`)}`);
   try {
     if (isNaN(+amount)) {
-      error(`Error: <AMOUNT> must be a number`);
+      error(`<AMOUNT> must be a number`);
       process.exit(1);
     }
     // Use GOT to make a POST request to the API
@@ -72,7 +67,12 @@ export async function add(...args) {
       },
     });
     // Log the result to the console
-    log(`Item "${id}:${name}" has been added to the ${category} category`);
+    log(
+      `${displaySuccess("Product Added! :")} `
+      + `${displayID(id)} ${displayText("-")} `
+      + `${displayName(name)} ${displayText("has been added to the")} `
+      + `${displayCategory(category)} ${displayText("category")}`
+    );
   } catch (err) {
     // If there is an error, log it to the console and exit
     error(err.message);
@@ -82,10 +82,14 @@ export async function add(...args) {
 
 // List the categories
 export function listCategories() {
-  log("Listing categories");
+  log(displayTimestamp());
+  log(displayInfo("Listing Categories"));
   try {
     // Loop through the categories and log them to the console
-    for (const cat of categories) log(cat);
+    log(displayText("Categories received from API:"));
+    for (const cat of categories) {
+      log(displayCategory(cat));
+    }
   } catch (err) {
     // If there is an error, log it to the console and exit
     error(err.message);
@@ -95,19 +99,25 @@ export function listCategories() {
 
 // List the IDs for the given category
 export async function listCategoryItems(category) {
-  log(`Listing IDs for category ${category}`);
+  log(displayTimestamp());
+  log(`${displayInfo(`List IDs`)}`);
+
   try {
     // Use GOT to make a GET request to the API
     const result = await got(`${API}/${category}/`).json();
     // Log the result to the console
+    log(`${displaySuccess("IDs received from API:")}`);
     for (const item of result) {
       log(
-        `${item.id}: ${item.name} - $${item.rrp}\nProduct Info:\t${item.info}`
+        `${displayKey("ID:")}\t${displayID(item.id)} `
+        + `${displayKey(`Name:`)}\t${displayName(item.name)} `
+        + `${displayKey("RRP:")}\t${displayRRP(item.rrp)} `
+        + `${displayKey("Product Info:")}\n\t${displayText(item.info)}`
       );
     }
   } catch (err) {
     // If there is an error, log it to the console and exit
-    console.log(err.message);
+    error(err.message);
     process.exit(1);
   }
 }
